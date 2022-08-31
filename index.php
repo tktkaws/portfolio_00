f4382ea4c204bc92e7cf4f2a9924257b1c04909c
takayuki-takahashi-dic <ttkaws15@gmail.com>
2022-09-01 07:38:55 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <header></header>
+    <section class="fv">
+    </section>
+    <section class="works">
+        <h2>WORKS</h2>
+    </section>
+    <section class="about">
+        <h2>ABOUT</h2>
+    </section>
+    <section class="blog">
+        <h2>BLOG</h2>
+    </section>
+    <section class="contact">
+        <h2>CONTACT</h2>
+    </section>
+    <footer></footer>
+
+</body>
+
+</html>
